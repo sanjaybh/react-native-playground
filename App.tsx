88a8faec2be12ffa0c6ecc7eb226b63ee3f5aa7e
Sanjay Bhan <Sanjay.Bhan@discounttire.com>
2023-10-10 10:18:53 -0400

    changes '

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import {
   ActivityIndicator,
   Button,
+  FlatList,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -16,6 +17,21 @@ const App = () => {
   const [isLoading, setLoading] = useState(true);
 
   const isDarkMode = useColorScheme() === 'dark';
+
+  const users = [
+    {id: 1, name: 'Sanjay'},
+    {id: 2, name: 'More'},
+    {id: 3, name: 'test'},
+    {id: 4, name: 'bogus'},
+    {id: 5, name: 'Sanjay'},
+    {id: 6, name: 'More'},
+    {id: 7, name: 'test'},
+    {id: 8, name: 'bogus'},
+    {id: 9, name: 'Sanjay'},
+    {id: 10, name: 'More'},
+    {id: 11, name: 'test'},
+    {id: 12, name: 'bogus'},
+  ];
 
   const getDataFromAPI = async () => {
     try {
@@ -34,7 +50,8 @@ const App = () => {
   }, []);
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? 'dark-content' : 'light-content',
+    // backgroundColor: isDarkMode ? 'dark-content' : 'light-content',
+    backgroundColor: '#dae773',
   };
 
   const handlePressMe = userName => {
@@ -44,15 +61,26 @@ const App = () => {
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        barStyle={isDarkMode ? 'dark-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
+
+      <View _style={style.flatListCont}>
+        <FlatList
+          data={users}
+          keyExtractor={(user, index) => user.id}
+          renderItem={user => (
+            <Text _style={style.flatListItems}>{user.name}</Text>
+          )}
+        />
+      </View>
+
       {isLoading ? (
         <ActivityIndicator />
       ) : (
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
-          style={backgroundStyle}>
+          style={style.backgroundStyle}>
           <View>
             <Text style={style.header}>Times of India</Text>
             <Text> </Text>
@@ -63,7 +91,9 @@ const App = () => {
               />
             </View>
           </View>
-          <Text>Total - {result?.total}</Text>
+          <Text style={{fontWeight: 'bold', padding: 10}}>
+            Total - {result?.total}
+          </Text>
           <View>
             {result.total
               ? result.posts.map(item => <ListItems item={item} />)
@@ -89,6 +119,27 @@ const ListItems = ({item}) => {
 export default App;
 
 const style = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: 'lightblue',
+  },
+  flatListCont: {
+    color: 'black',
+    textAlign: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'blue',
+  },
+  flatListItems: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'green',
+    textAlign: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'orange',
+    padding: 10,
+    margin: 10,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
   header: {
     fontSize: 20,
     fontWeight: 'bold',
@@ -106,9 +157,12 @@ const style = StyleSheet.create({
     justifyContent: 'center',
   },
   itemListContainer: {
-    padding: 7,
-    margin: 5,
-    backgroundColor: '#fff',
+    padding: 10,
+    margin: 10,
+    backgroundColor: '#f5f7ec',
+    borderBottomColor: '#000',
+    borderWidth: 0,
+    borderRadius: 10,
   },
   itemList: {
     fontSize: 15,

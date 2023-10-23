202f3af7d828a2d814e345d11679d1b60685f5cc
Sanjay Bhan <Sanjay.Bhan@discounttire.com>
2023-10-23 19:44:46 -0400

    added more comp

--- App.tsx
+++ App.tsx
@@ -1,9 +1,23 @@
 import {
   Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
   Text,
-  View
+  View,
+  useColorScheme
 } from 'react-native';
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 import React, {useEffect, useState} from 'react';
+
+import {countryListObj} from './Constants/index'
 
 //import API_JsonServer from './components/API_JsonServer';
 
@@ -25,9 +39,68 @@ import React, {useEffect, useState} from 'react';
 
 
 function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'light';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    marginTop:1
+  };
+
   return (
-    <Text>Hello World!</Text>
+    <SafeAreaView>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic">
+        {countryListObj.map((text, index) => (
+          <InnerComp text={text} index={index} />
+        ))}
+
+      </ScrollView>
+      </SafeAreaView>
   );
 }
+
+const InnerComp = ({text, index}) =>{
+  return (
+    <View style={styles.mainContainer} key={index}>
+      <Text style={styles.sectionContainer}>
+        <Text style={styles.secText}>Name: </Text>
+        <Text style={styles.secTextName}>{text.name}</Text>
+        </Text>
+      <Text style={styles.sectionContainer}>
+        <Text style={styles.secText}>Currency: </Text>
+          {text.currency}
+        </Text>
+      <Text style={styles.sectionContainer}>
+        <Text style={styles.secText}>Capital: </Text>
+          {text.capital}
+        </Text>
+      <Text style={styles.sectionContainer}>
+        <Text style={styles.secText}>Continent: </Text>
+          {text.continent}
+        </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  mainContainer:{
+    padding:5, margin:1, backgroundColor:'#cecaca'
+  },
+  sectionContainer: {
+    marginTop: 0,
+    paddingHorizontal: 1,
+    color:'#000'
+  },
+  secText:{
+    fontWeight:'bold'
+  },
+  secTextName:{
+    color:'blue'
+  }
+});
 
 export default App;

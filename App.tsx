bdfd23b35f279e09e692da90ba4c217692703ddf
Sanjay Bhan <Sanjay.Bhan@discounttire.com>
2023-10-25 00:04:25 -0400

    added more comp

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   View,
   useColorScheme
 } from 'react-native';
@@ -15,9 +16,9 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import React, {useEffect, useState} from 'react';
+import React, {Suspense, useEffect, useState} from 'react';
 
-import {countryListObj} from './Constants/index'
+//import {countryListObj} from './Constants/index';
 
 //import API_JsonServer from './components/API_JsonServer';
 
@@ -36,51 +37,148 @@ import {countryListObj} from './Constants/index'
 
 //import FlatlistComp from './components/FlatlistComp';
 //import FlexBox from './components/FlexBox';
+let countryListObj = [
+	{
+	 "id": 1,
+	 "name": "India",
+	 "currency": "INR",
+	 "phone": 93,
+	 "capital": "New Delhi",
+	 "code": "In",
+	 "code3": "Ind",
+	 "continent": "Asia",
+	 "number": 4
+	},
+	{
+	 "id": 2,
+	 "name": "Indo",
+	 "currency": "ZAR",
+	 "phone": 27,
+	 "capital": "Pretoria",
+	 "code": "ZA",
+	 "code3": "ZAF",
+	 "continent": "África",
+	 "number": 710
+	},
+	{
+	 "id": 3,
+	 "name": "Zim",
+	 "currency": "ALL",
+	 "phone": 355,
+	 "capital": "Tirana",
+	 "code": "AL",
+	 "code3": "ALB",
+	 "continent": "Europa",
+	 "number": 8
+	}]
 
 
 function App(): JSX.Element {
+  const [countriesList, setCountriesList] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+    const [users, setUsers] = useState([]);
+    
   const isDarkMode = useColorScheme() === 'light';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
     marginTop:1
   };
+  // useEffect(()=>{
+  //   async function loadData(){
+  //     const {countryListObj} = await import(`./Constants/index`);
+  //     setCountriesList([{
+  //       "id": 1,
+  //       "name": "Afeganistão",
+  //       "currency": "AFN",
+  //       "phone": 93,
+  //       "capital": "Kabul",
+  //       "code": "AF",
+  //       "code3": "AFG",
+  //       "continent": "Ásia",
+  //       "number": 4
+  //      }]);
+  //     //import(`./${bird}.json`).then(data => setInitial(data));
+  //   }
+  
+  //   loadData();
+  // },[])
 
+  useEffect(()=>{
+    setCountriesList(countryListObj)
+  }, []);
+
+  function handleSeach(text){ 
+    let filteredList = countriesList.filter((item) => {
+      //return (item.name).toLowerCase() == (text).toLowerCase()
+
+      return (item.name).toLowerCase().includes(text.toLowerCase())
+    });
+
+    //author.toLowerCase().includes(searchText.toLowerCase())
+
+    console.log("==> "+JSON.stringify(filteredList));
+    
+    if(filteredList.length > 0){
+      setCountriesList(filteredList)
+    }    
+    if(text == ''){
+      setCountriesList(countryListObj)
+    }
+  }
+
+  const getUsers = () => {
+      fetch('https://jsonplaceholder.typicode.com/users/')
+        .then((response) => response.json())
+        .then((json) => setUsers(json))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+  }
+  
   return (
     <SafeAreaView>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
+      <TextInput
+            style={{height: 40, borderRadius:10, backgroundColor:'#f5f0f0', margin:4, borderWidth:1}}
+            placeholder="Type here to search!"
+            // clearButtonMode='always'
+            // autoCapitalize='none'
+            // autoCorrect={false}
+            // value={searchQuery}
+            onChangeText={newText => handleSeach(newText)}
+          />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic">
-        {countryListObj.map((text, index) => (
-          <InnerComp text={text} index={index} />
-        ))}
-
+        {
+          countriesList.map((txt, index) => <InnerComp txt={txt} index={index} key={index} />)
+        }
       </ScrollView>
-      </SafeAreaView>
+    </SafeAreaView>
   );
 }
 
-const InnerComp = ({text, index}) =>{
+const InnerComp = ({txt, index}) =>{
   return (
     <View style={styles.mainContainer} key={index}>
       <Text style={styles.sectionContainer}>
         <Text style={styles.secText}>Name: </Text>
-        <Text style={styles.secTextName}>{text.name}</Text>
+        <Text style={styles.secTextName}>{txt.name}</Text>
         </Text>
       <Text style={styles.sectionContainer}>
         <Text style={styles.secText}>Currency: </Text>
-          {text.currency}
+          {txt.currency}
         </Text>
       <Text style={styles.sectionContainer}>
         <Text style={styles.secText}>Capital: </Text>
-          {text.capital}
+          {txt.capital}
         </Text>
       <Text style={styles.sectionContainer}>
         <Text style={styles.secText}>Continent: </Text>
-          {text.continent}
+          {txt.continent}
         </Text>
     </View>
   )
@@ -88,7 +186,9 @@ const InnerComp = ({text, index}) =>{
 
 const styles = StyleSheet.create({
   mainContainer:{
-    padding:5, margin:1, backgroundColor:'#cecaca'
+    padding:5, margin:1, backgroundColor:'#f5f0f0',
+    borderWidth:1,
+    borderColor:'pink'
   },
   sectionContainer: {
     marginTop: 0,
@@ -100,6 +200,14 @@ const styles = StyleSheet.create({
   },
   secTextName:{
     color:'blue'
+  },
+  textInputContainer:{},
+  textInput:{
+    borderWidth:1,
+    borderColor:'green',
+    height: 40,
+    borderRadius:10,
+    width:50
   }
 });
 
